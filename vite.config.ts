5482b9f0dc6989e255171f18e25fd28f980492f5
Waelsa10 <104719029+Waelsa10@users.noreply.github.com>
2025-11-09 10:47:16 +0300

    Update API keys in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyDkx2S0UqIR2ks0fuGrDDhUIWY_alYH7Rg),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyDkx2S0UqIR2ks0fuGrDDhUIWY_alYH7Rg)
       },
       resolve: {
         alias: {
